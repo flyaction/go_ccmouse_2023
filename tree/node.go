af6e36d3ee2ee01fa559c4dd9a9a17cca935d408
action <xingdong@soyoung.com>
2023-04-05 14:57:13 +0800

    package

--- tree/node.go
+++ tree/node.go
@@ -19,15 +19,6 @@ func (node *Node) SetValue(value int) {
 	node.Value = value
 }
 
-func (node *Node) Traverse() {
-	if node == nil {
-		return
-	}
-	node.Left.Traverse()
-	node.Print()
-	node.Right.Traverse()
-}
-
 func CreateNode(value int) *Node {
 	return &Node{Value: value}
 }
