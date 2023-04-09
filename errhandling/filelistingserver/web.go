5d1b7c997c6f5e6ad9a458590d8d6734c9ebc827
action <xingdong@soyoung.com>
2023-04-09 19:52:46 +0800

    error+panic+recover

--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -13,9 +13,24 @@ type appHandler func(writer http.ResponseWriter, request *http.Request) error
 func errWrapper(handle appHandler) func(http.ResponseWriter, *http.Request) {
 
 	return func(writer http.ResponseWriter, request *http.Request) {
+
+		defer func() {
+			r := recover()
+			if r != nil {
+				log.Printf("Panic: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+
 		err := handle(writer, request)
 		if err != nil {
 			log.Printf("Error handling request: %s", err.Error())
+
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -31,8 +46,13 @@ func errWrapper(handle appHandler) func(http.ResponseWriter, *http.Request) {
 
 }
 
+type userError interface {
+	error
+	Message() string
+}
+
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.HandleFileList))
+	http.HandleFunc("/", errWrapper(filelisting.HandleFileList))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
