f6a138f250ee4727993679282cb071007f66e7df
action <xingdong@soyoung.com>
2023-04-09 14:20:00 +0800

    file server

---
+++ errhandling/filelistingserver/web.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/list/", func(writer http.ResponseWriter, request *http.Request) {
+		path := request.URL.Path[len("/list/"):]
+		file, err := os.Open(path)
+		if err != nil {
+			panic(err)
+		}
+		defer file.Close()
+
+		all, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+		writer.Write(all)
+	})
+
+	http.ListenAndServe(":8989", nil)
+}
