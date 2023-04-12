81533d5663912fdb7eea68efbb7b8415baf5cefb
action <xingdong@soyoung.com>
2023-04-12 12:00:15 +0800

    testing.B

--- container/nonrepeating/nonrepeating_test.go
+++ container/nonrepeating/nonrepeating_test.go
@@ -1,6 +1,12 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
+
+//代码覆盖率
+//go test -coverprofile=c.out
+//go tool cover -html=c.out  // go tool cover 查看其他查看方式
 
 func TestSubstr(t *testing.T) {
 
@@ -24,6 +30,25 @@ func TestSubstr(t *testing.T) {
 		actual := lengthOfNonRepeatingSubStr(tt.s)
 		if tt.ans != actual {
 			t.Errorf("get %d for input %s; expected %d", tt.ans, tt.s, actual)
+		}
+	}
+}
+
+//性能测试 go test -bench .
+func BenchmarkSubstr(b *testing.B) {
+	s := "黑化肥挥发发灰会花飞灰化肥挥发发黑会飞花"
+	for i := 0; i < 13; i++ {
+		s += s
+	}
+	ant := 8
+
+	b.Logf("len(s)=%d", len(s))
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		actual := lengthOfNonRepeatingSubStr(s)
+		if actual != ant {
+			b.Errorf("get %d for input %s; expected %d", ant, s, actual)
 		}
 	}
 }
