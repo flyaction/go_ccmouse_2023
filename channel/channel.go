381ed02f3250146a1be881a51b8776ca3410662f
action <xingdong@soyoung.com>
2023-04-13 13:45:11 +0800

    channel-缓冲区

--- channel/channel.go
+++ channel/channel.go
@@ -5,19 +5,22 @@ import (
 	"time"
 )
 
-func creatWorker(id int) chan int {
+func worker(id int, c chan int) {
+
+	for {
+		fmt.Printf("Worker %d received %c\n", id, <-c)
+	}
+}
+
+func creatWorker(id int) chan<- int {
 
 	c := make(chan int)
-	go func() {
-		for {
-			fmt.Printf("Worker %d received %c\n", id, <-c)
-		}
-	}()
+	go worker(id, c)
 	return c
 
 }
 func chanDemo() {
-	var channel [10]chan int
+	var channel [10]chan<- int
 	for i := 0; i < 10; i++ {
 		channel[i] = creatWorker(i)
 
@@ -34,6 +37,17 @@ func chanDemo() {
 
 }
 
+func bufferedChannel() {
+	c := make(chan int, 3)
+	go worker(0, c)
+	c <- 'a'
+	c <- 'b'
+	c <- 'c'
+	c <- 'd'
+	time.Sleep(time.Millisecond)
+}
+
 func main() {
-	chanDemo()
+	//chanDemo()
+	bufferedChannel()
 }
