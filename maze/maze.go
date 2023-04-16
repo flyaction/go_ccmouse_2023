90eaaa26cfb623780fe83aca63888015407abbec
action <xingdong@soyoung.com>
2023-04-16 11:12:16 +0800

    迷宫代码实现-walk

--- maze/maze.go
+++ maze/maze.go
@@ -25,6 +25,70 @@ func readMaze(filename string) [][]int {
 
 }
 
+type point struct {
+	i, j int
+}
+
+// x + y轴
+var dirs = [4]point{
+	{-1, 0}, {0, -1}, {1, 0}, {0, 1},
+}
+
+func (p point) add(r point) point {
+	return point{p.i + r.i, p.j + r.j}
+}
+
+func (p point) at(grid [][]int) (int, bool) {
+	if p.i < 0 || p.j >= len(grid) {
+		return 0, false
+	}
+	if p.j < 0 || p.j >= len(grid[p.i]) {
+		return 0, false
+	}
+
+	return grid[p.i][p.j], true
+}
+
+func walk(maze [][]int, start, end point) {
+	steps := make([][]int, len(maze))
+	for i := range steps {
+		steps[i] = make([]int, len(maze[i]))
+	}
+
+	Q := []point{start}
+
+	for len(Q) > 0 {
+		cur := Q[0]
+		Q = Q[1:]
+		for _, dir := range dirs {
+			next := cur.add(dir)
+
+			// maze at next is 0
+			// and steps as next is 0
+			// and next != start
+			val, ok := next.at(maze)
+			if !ok || val == 1 {
+				continue
+			}
+
+			val, ok = next.at(steps)
+			if !ok || val != 0 {
+				continue
+			}
+
+			if next == start {
+				continue
+			}
+
+			curSteps, _ := cur.at(steps)
+			steps[next.i][next.j] = curSteps + 1
+			Q = append(Q, next)
+
+		}
+	}
+
+}
+
 func main() {
 
 	maze := readMaze("maze/maze.in")
