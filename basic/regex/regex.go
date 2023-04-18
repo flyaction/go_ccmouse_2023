5ff60e2b787fd42e292a729b6d9133122d4ed138
action <xingdong1117@126.com>
2023-04-18 13:26:48 +0800

    regex-正则

--- basic/regex/regex.go
+++ basic/regex/regex.go
@@ -5,12 +5,16 @@ import (
 	"regexp"
 )
 
-const text = "My email is ccmouse@gmail.com"
+const text = `My email is ccmouse@gmail.com
+email2 is abc@qq.com
+email3 is ccc@126.com
+end
+`
 
 func main() {
-	re := regexp.MustCompile("[a-z0-9A-Z]+@[a-z0-9A-Z]+\\.[a-z0-9A-Z]+")
+	re := regexp.MustCompile("[a-z0-9A-Z]+@[a-z0-9A-Z.]+\\.[a-z0-9A-Z]+")
 
-	math := re.FindString(text)
+	math := re.FindAllString(text, -1)
 
 	fmt.Println(re, "\n", math)
 }
