1befa6a336d2cb00f5e673bceb23a8f563642ceb
action <xingdong1117@126.com>
2023-04-23 12:29:49 +0800

    爬虫-重构+总结

--- crawler/scheduler/queued.go
+++ crawler/scheduler/queued.go
@@ -9,6 +9,10 @@ type QueuedScheduler struct {
 	workerChan  chan chan engine.Request
 }
 
+func (s *QueuedScheduler) WorkerChan() chan engine.Request {
+	return make(chan engine.Request)
+}
+
 func (s *QueuedScheduler) Submit(r engine.Request) {
 	s.requestChan <- r
 
@@ -18,9 +22,9 @@ func (s *QueuedScheduler) WorkerReady(w chan engine.Request) {
 	s.workerChan <- w
 }
 
-func (s *QueuedScheduler) ConfigureMasterWorkerChan(chan engine.Request) {
-	//panic("implement me")
-}
+//func (s *QueuedScheduler) ConfigureMasterWorkerChan(chan engine.Request) {
+//	//panic("implement me")
+//}
 
 func (s *QueuedScheduler) Run() {
 
