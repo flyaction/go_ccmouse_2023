11eb87496f8177bcaffb967dba35bfb633d4a3b7
action <xingdong1117@126.com>
2023-04-25 21:27:07 +0800

    fix

--- crawler_distributed/main.go
+++ crawler_distributed/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	worker "imooc.com/ccmouse/learngo/crawler_distributed/worker/client"
 
 	"imooc.com/ccmouse/learngo/crawler/jiaou/parserjiaou"
 	"imooc.com/ccmouse/learngo/crawler_distributed/config"
-	"imooc.com/ccmouse/learngo/crawler_distributed/persist/client"
+	itemsaver "imooc.com/ccmouse/learngo/crawler_distributed/persist/client"
 
 	"imooc.com/ccmouse/learngo/crawler/scheduler"
 
@@ -14,15 +17,23 @@ import (
 
 func main() {
 
-	itemChan, err := client.ItemSaver(fmt.Sprintf(":%d", config.ItemSaverPort))
+	itemChan, err := itemsaver.ItemSaver(fmt.Sprintf(":%d", config.ItemSaverPort))
 	if err != nil {
 		//panic(err)
+		log.Fatal(err)
+	}
+
+	processor, err := worker.CreateProcessor()
+	if err != nil {
+		//panic(err)
+		log.Fatal(err)
 	}
 
 	e := engine.ConcurrentEngine{
-		Scheduler:   &scheduler.QueuedScheduler{},
-		WorkerCount: 10,
-		ItemChan:    itemChan,
+		Scheduler:        &scheduler.QueuedScheduler{},
+		WorkerCount:      10,
+		ItemChan:         itemChan,
+		RequestProcessor: processor,
 	}
 	//e.Run(engine.Request{
 	//	Url:        "https://www.zhenai.com/zhenghun",
